7e383471599ea1f778cc0ca762ef9c9957e0c1d3
Ostanin Roman <ostanin.roma15@gmail.com>
2024-04-17 22:10:01 +0700

    fix and add code, add comment

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,81 +1,85 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
 Win::Win(QWidget *parent):QWidget(parent)
 {
-    codec = QTextCodec::codecForName("Windows-1251");
-    setWindowTitle(codec->toUnicode("Возведение в квадрат"));
-    frame = new QFrame(this);
-    frame->setFrameShadow(QFrame::Raised);
-    frame->setFrameShape(QFrame::Panel);
-    inputLabel = new QLabel(codec->toUnicode("Введите число:"),
-                            this);
-    inputEdit = new QLineEdit("",this);
-    StrValidator *v=new StrValidator(inputEdit);
-    inputEdit->setValidator(v);
-    outputLabel = new QLabel(codec->toUnicode("Результат:"),
-                             this);
-    outputEdit = new QLineEdit("",this);
-    nextButton = new QPushButton(codec->toUnicode("Следующее"),
-                                 this);
-    exitButton = new QPushButton(codec->toUnicode("Выход"),
-                                 this);
+    setWindowTitle("Возведение в квадрат");
+    frame = new QFrame(this); //устанавливаем заголовок окна
+    frame->setFrameShadow(QFrame::Raised); //устанавливаем стиль тени для рамки обьекта qframe
+    frame->setFrameShape(QFrame::Panel); //устанавливаем стиль формы
+    inputLabel = new QLabel("Введите число:", this); //создаем обьект qlable с текстом
+    inputEdit = new QLineEdit("",this); //создаем обьект qlineedite для ввода текста
+    StrValidator *v=new StrValidator(inputEdit); //создаем обьект валидатоа для проверки данных
+    inputEdit->setValidator(v); //
+    outputLabel = new QLabel("Результат:", this); //создаем обьект qlabel с текстом
+    outputEdit = new QLineEdit("",this); //создаем обьект qlineedite для вывода текста
+    nextButton = new QPushButton("Следующее", this); //создаем обьект кнопку
+    exitButton = new QPushButton("Выход", this);
+
     // компоновка приложения выполняется согласно рисунку 2.
-    QVBoxLayout *vLayout1 = new QVBoxLayout(frame);
-    vLayout1->addWidget(inputLabel);
+    QVBoxLayout *vLayout1 = new QVBoxLayout(frame); //создаем вертикальную компановку для обьекта frame
+    vLayout1->addWidget(inputLabel); //добавляем обьект в компановку
     vLayout1->addWidget(inputEdit);
     vLayout1->addWidget(outputLabel);
     vLayout1->addWidget(outputEdit);
-    vLayout1->addStretch();
-    QVBoxLayout *vLayout2 = new QVBoxLayout();
-    vLayout2->addWidget(nextButton);
+    vLayout1->addStretch(); //добавляет растягиваемое окно в компановку vloyout1
+
+    QVBoxLayout *vLayout2 = new QVBoxLayout(); //создаем вертикальную компановку
+    vLayout2->addWidget(nextButton); //добавляем обьект в компановку
     vLayout2->addWidget(exitButton);
-    vLayout2->addStretch();
-    QHBoxLayout *hLayout = new QHBoxLayout(this);
-    hLayout->addWidget(frame);
+    vLayout2->addStretch(); //добавляет растягиваемое окно в компановку vloyout2
+
+    QHBoxLayout *hLayout = new QHBoxLayout(this); //создаем горизонтальную компановку
+    hLayout->addWidget(frame); //добавляем обьект в компановку
     hLayout->addLayout(vLayout2);
     begin();
-    connect(exitButton,SIGNAL(clicked(bool)),
-            this,SLOT(close()));
-    connect(nextButton,SIGNAL(clicked(bool)),
-            this,SLOT(begin()));
-    connect(inputEdit,SIGNAL(returnPressed()),
-            this,SLOT(calc()));
+
+    //устанавливает связь между сигналом clicked от кнопки exitbutton и слотом close в обьекте класса Win
+    connect(exitButton, &QPushButton::clicked, this, &Win::close);
+
+    //устанавливает связь между сигналом clicked от кнопки nextbutton и слотом begin в обьекте класса Win
+    connect(nextButton, &QPushButton::clicked, this, &Win::begin);
+
+    //устанавливает связь между сигналом returnpressed от виджета inputEdit и слотом calc в обьекте класса Win
+    connect(inputEdit, &QLineEdit::returnPressed, this, &Win::calc);
 }
-void Win::begin()
+
+void Win::begin() // метод начальной настройки интерфейса
 {
-    inputEdit->clear();
-    nextButton->setEnabled(false);
-    nextButton->setDefault(false);
-    inputEdit->setEnabled(true);
-    outputLabel->setVisible(false);
-    outputEdit->setVisible(false);
-    outputEdit->setEnabled(false);
-    inputEdit->setFocus();
+    inputEdit->clear(); // очищает поле ввода
+    nextButton->setEnabled(false); //делаем недоступным кнопку nextButton
+    nextButton->setDefault(false); //задаем значение по умолчанию(кнопка не выделяется)
+    inputEdit->setEnabled(true); //поле ввода доступно
+    outputLabel->setVisible(false); //обьект outputLabel делаем невидимым
+    outputEdit->setVisible(false); //обьект outputEdit делаем невидимым
+    outputEdit->setEnabled(false); //не дает редактировать поле с результатом
+    inputEdit->setFocus(); //предоставляет фокус ввода с клавиатуры этому обьекту
 }
+
 void Win::calc()
 {
-    bool Ok=true; float r,a;
-    QString str=inputEdit->text();
-    a=str.toDouble(&Ok);
+    bool Ok = true;
+    float r,a;
+    QString str = inputEdit->text(); //создаем обьект str задаем его равным
+    a = str.toDouble(&Ok); //преобразовываем и записываем результат, если произошла ошибка
+                            //значение ok изменится
     if (Ok)
     {
         r=a*a;
-        str.setNum(r);
-        outputEdit->setText(str);
-        inputEdit->setEnabled(false);
-        outputLabel->setVisible(true);
-        outputEdit->setVisible(true);
-        nextButton->setDefault(true);
-        nextButton->setEnabled(true);
-        nextButton->setFocus();
+        str.setNum(r); //устанавливает str значение r
+        outputEdit->setText(str); //записываем в outputEdit str(результат)
+        inputEdit->setEnabled(false); //inputEdit недоступно
+        outputLabel->setVisible(true); //outputLabel видимо
+        outputEdit->setVisible(true); //outputEdit видимо
+        nextButton->setDefault(true); //nextButton выделяется(можно нажать)
+        nextButton->setEnabled(true); //nextButton доступна
+        nextButton->setFocus(); //фокус отдается nextButton
     }
     else
         if (!str.isEmpty())
         {
             QMessageBox msgBox(QMessageBox::Information,
-                               codec->toUnicode("Возведение в квадрат."),
-                               codec->toUnicode("Введено неверное значение."),
+                               ("Возведение в квадрат."),
+                               ("Введено неверное значение."),
                                QMessageBox::Ok);
             msgBox.exec();
         }
--- mainwindow.h
+++ mainwindow.h
@@ -1,14 +1,14 @@
-#ifndef MAINWINDOW_H
+#ifndef MAINWINDOW_H //директива для предотвращения многократного включения файла
 #define MAINWINDOW_H
-#include <QtGui>
 
-#include <QWidget>
+#include <QtWidgets> //библиотека для всех обьектов пользовательского интерфейса
 
 class Win:public QWidget // класс окна
 {
     Q_OBJECT // макрос Qt, обеспечивающий корректное создание сигналов и слотов
+
 protected:
-    QTextCodec *codec;
+    //обьявление указателей на различные виджеты, которые будут использоваться в пользовательском интерфейсе окна
     QFrame *frame; // рамка
     QLabel *inputLabel; // метка ввода
     QLineEdit *inputEdit; // строчный редактор ввода
@@ -16,18 +16,29 @@ protected:
     QLineEdit *outputEdit; // строчный редактор вывода
     QPushButton *nextButton; // кнопка Следующее
     QPushButton *exitButton; // кнопка Выход
+
 public:
     Win(QWidget *parent = 0); // конструктор
+
 public slots:
+    //методы класса
     void begin(); // метод начальной настройки интерфейса
     void calc(); // метод реализации вычислений
 };
+
 class StrValidator:public QValidator // класс компонента проверки ввода
 {
 public:
-    StrValidator(QObject *parent):QValidator(parent){}
-    virtual State validate(QString &str,int &pos)const
+    StrValidator(QObject *parent):QValidator(parent){} //конструктор наследника с вызовом конструктора базового класса
+    State validate(QString &str,int &pos)const override //метод проверки вводимой строки
     {
+        auto msg = QMessageBox(
+                    QMessageBox::Information,
+                    "Предупреждение",
+                    "Вызван метод validate",
+                    QMessageBox::Ok
+                );
+                msg.exec();
         return Acceptable; // метод всегда принимает вводимую строку
     }
 };
